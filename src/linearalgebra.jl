511e393d884a2973888ce43d30ea99d7484ff3d5
Michael Helton <heltonmc@protonmail.com>
2023-04-20 10:07:27 -0400

    add general method

--- src/linearalgebra.jl
+++ src/linearalgebra.jl
@@ -1,43 +1,20 @@
-function dot(x::NTuple{N, Complex{T}}, y::NTuple{N, Complex{T}}) where {N, T}
-    R = N % 4
+dot(x::NTuple{N, T}, y::NTuple{N, T}) where {N, T <: Union{Float16, ComplexF16}} = dot(x, y, Val(8))
+dot(x::NTuple{N, T}, y::NTuple{N, T}) where {N, T} = dot(x, y, Val(4))
 
-    if R == 0
-        _x = constantvector(zero(Complex{T}), ComplexVec{4, T})
-        _y = constantvector(zero(Complex{T}), ComplexVec{4, T})
-    elseif R == 1
-        _x = ComplexVec((x[1], zero(Complex{T}), zero(Complex{T}), zero(Complex{T})))
-        _y = ComplexVec((y[1], zero(Complex{T}), zero(Complex{T}), zero(Complex{T})))
-    elseif R == 2
-        _x = ComplexVec((x[1], x[2], zero(Complex{T}), zero(Complex{T})))
-        _y = ComplexVec((y[1], y[2], zero(Complex{T}), zero(Complex{T})))
-    elseif R == 3
-        _x = ComplexVec((x[1], x[2], x[3], zero(Complex{T})))
-        _y = ComplexVec((y[1], y[2], y[3], zero(Complex{T})))
-    end
-
-    out = fmul(conj(_x), _y)
-
-    for i in R+1:4:length(x)
-        a = ComplexVec((x[i], x[i+1], x[i+2], x[i+3]))
-        b = ComplexVec((y[i], y[i+1], y[i+2], y[i+3]))
-        out = fmadd(conj(a), b, out)
-    end
-    return fhadd(out)
-end
-
-function dot(x::NTuple{N, T}, y::NTuple{N, T}) where {N, T}
-    unroll = 32
+function dot(x::NTuple{N, T}, y::NTuple{N, T}, ::Val{M}) where {N, T, M}
+    V = T <: Real ? Vec : ComplexVec
+    unroll = M
     R = N % unroll
 
-    _x = Vec((ntuple(i -> x[i], Val(R))..., ntuple(i -> zero(T), Val(unroll-R))...))
-    _y = Vec((ntuple(i -> y[i], Val(R))..., ntuple(i -> zero(T), Val(unroll-R))...))
+    _x = (ntuple(i -> x[i], Val(R))..., ntuple(i -> zero(T), Val(unroll-R))...)
+    _y = (ntuple(i -> y[i], Val(R))..., ntuple(i -> zero(T), Val(unroll-R))...)
   
-    out = fmul(_x, _y)
+    out = fmul(conj(V(_x)), V(_y))
 
     for i in R+1:unroll:length(x)
-        a = Vec(ntuple(k -> x[i + k - 1], Val(unroll)))
-        b = Vec(ntuple(k -> y[i + k - 1], Val(unroll)))
-        out = fmadd(a, b, out)
+        a = ntuple(k -> x[i + k - 1], Val(unroll))
+        b = ntuple(k -> y[i + k - 1], Val(unroll))
+        out = fmadd(conj(V(a)), V(b), out)
     end
     return fhadd(out)
 end
